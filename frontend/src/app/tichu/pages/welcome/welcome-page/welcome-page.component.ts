d40f7b0294a83f031192b5579784f6bccadea7c7
jaunerc <cyrill.jauner@zuehlke.com>
2023-08-03 08:30:56 +0200

    add cors configuration to enable requests from the frontend to the backend on localhost

--- frontend/src/app/tichu/pages/welcome/welcome-page/welcome-page.component.ts
+++ frontend/src/app/tichu/pages/welcome/welcome-page/welcome-page.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Player, PlayersService} from "../../../api";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
-export class WelcomePageComponent {
+export class WelcomePageComponent implements OnInit {
 
+  public player$!: Observable<Player>;
+
+  constructor(
+    private playersService: PlayersService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.player$ = this.playersService.createPlayer("Champion");
+  }
 }
