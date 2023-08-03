a473655058f5d32fb0165100d54a22a5ac74527a
jaunerc <cyrill.jauner@zuehlke.com>
2023-08-03 10:09:43 +0200

    set the base path for api request to localhost

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,10 +1,17 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {ApiModule} from "./tichu/api";
-import {HttpClientModule} from "@angular/common/http";
+import {AppRoutingModule} from './app-routing.module'
+import {AppComponent} from './app.component'
+import {ApiModule, Configuration, ConfigurationParameters} from './tichu/api'
+import {HttpClientModule} from '@angular/common/http'
+
+export function apiConfiguration(): Configuration {
+  const params: ConfigurationParameters = {
+    basePath: 'http://localhost:8080/api'
+  }
+  return new Configuration(params)
+}
 
 @NgModule({
   declarations: [
@@ -13,8 +20,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ApiModule,
-    HttpClientModule,
+    ApiModule.forRoot(apiConfiguration),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
