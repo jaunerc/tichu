09b2914cecb70b42145af4a83c95dc1f464271bf
jaunerc <cyrill.jauner@zuehlke.com>
2023-08-04 10:20:23 +0200

    add h2 database in a new persistence module

--- backend/app/src/main/java/ch/jaunerc/tichu/backend/app/TichuBackendApplication.java
+++ backend/app/src/main/java/ch/jaunerc/tichu/backend/app/TichuBackendApplication.java
@@ -1,13 +1,14 @@
 package ch.jaunerc.tichu.backend.app;
 
 import ch.jaunerc.tichu.backend.domain.DomainConfiguration;
+import ch.jaunerc.tichu.backend.persistence.PersistenceConfiguration;
 import ch.jaunerc.tichu.backend.web.WebConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({WebConfiguration.class, DomainConfiguration.class})
+@Import({WebConfiguration.class, DomainConfiguration.class, PersistenceConfiguration.class})
 public class TichuBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(TichuBackendApplication.class, args);
---
+++ backend/persistence/src/main/java/ch/jaunerc/tichu/backend/persistence/PersistenceConfiguration.java
@@ -0,0 +1,9 @@
+package ch.jaunerc.tichu.backend.persistence;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class PersistenceConfiguration {
+}
