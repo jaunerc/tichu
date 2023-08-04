b026269a3e62e1f2c7d9ac8af257e7b7a5780297
jaunerc <cyrill.jauner@zuehlke.com>
2023-08-04 14:08:00 +0200

    add jpa annotations in order to scan the right packages for repositories and entities

--- backend/persistence/src/main/java/ch/jaunerc/tichu/backend/persistence/PersistenceConfiguration.java
+++ backend/persistence/src/main/java/ch/jaunerc/tichu/backend/persistence/PersistenceConfiguration.java
@@ -1,9 +1,13 @@
 package ch.jaunerc.tichu.backend.persistence;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan
+@EnableJpaRepositories(basePackages = {"ch.jaunerc.tichu.backend.persistence"})
+@EntityScan("ch.jaunerc.tichu.backend.persistence")
 public class PersistenceConfiguration {
 }
