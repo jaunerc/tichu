b6deefe17ea99b013860924c7ca26313af080a03
jaunerc <cyrill.jauner@zuehlke.com>
2023-08-06 15:48:15 +0200

    apply eslint

--- frontend/src/app/tichu/pages/welcome/welcome.module.ts
+++ frontend/src/app/tichu/pages/welcome/welcome.module.ts
@@ -1,11 +1,11 @@
-import {NgModule} from '@angular/core'
-import {CommonModule} from '@angular/common'
-import {WelcomePageComponent} from './welcome-page/welcome-page.component'
-import {WelcomeRoutingModule} from './welcome-routing.module'
-import {MatToolbarModule} from '@angular/material/toolbar'
-import {MatInputModule} from '@angular/material/input'
-import {MatButtonModule} from '@angular/material/button'
-import {ReactiveFormsModule} from '@angular/forms'
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { WelcomePageComponent } from './welcome-page/welcome-page.component'
+import { WelcomeRoutingModule } from './welcome-routing.module'
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { ReactiveFormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [
