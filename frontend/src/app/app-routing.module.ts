573e64fe1a0161c9b91259705c8915866683500d
jaunerc <cyrill.jauner@zuehlke.com>
2023-08-08 09:48:16 +0200

    add guard to restrict the access to the lobby page
    It should only be possible to access the lobby page if the username is in the store.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule, Type } from '@angular/core'
 import { Route, RouterModule } from '@angular/router'
 import { WelcomeModule } from './tichu/pages/welcome/welcome.module'
 import { LobbyModule } from './tichu/pages/lobby/lobby.module'
+import { userRegisteredGuard } from './tichu/guards/user-registered.guard'
 
 export type RoutePath =
   | ''
@@ -20,7 +21,8 @@ const routes: TichuRoute[] = [
   {
     path: 'lobby',
     loadChildren: async (): Promise<Type<LobbyModule>> =>
-      await import('./tichu/pages/lobby/lobby.module').then((m) => m.LobbyModule)
+      await import('./tichu/pages/lobby/lobby.module').then((m) => m.LobbyModule),
+    canActivate: [userRegisteredGuard]
   }
 ]
 
---
+++ frontend/src/app/tichu/guards/user-registered.guard.ts
@@ -0,0 +1,15 @@
+import { CanActivateFn, Router } from '@angular/router'
+import { inject } from '@angular/core'
+import { Store } from '@ngrx/store'
+import { getUsername } from '../states/app/app.selector'
+import { map } from 'rxjs'
+
+export const userRegisteredGuard: CanActivateFn = (route, state) => {
+  const store = inject(Store)
+  const router = inject(Router)
+  return store
+    .select(getUsername)
+    .pipe(
+      map((username) => !(username == null) ? true : router.parseUrl('/'))
+    )
+}
