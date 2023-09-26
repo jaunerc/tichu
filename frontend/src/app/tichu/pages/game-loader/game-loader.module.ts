a146d9c23ad81607589755dcdba23f81f4452543
jaunerc <c.jauner@gmail.com>
2023-09-26 22:05:58 +0200

    apply eslint

--- frontend/src/app/tichu/pages/game-loader/game-loader.module.ts
+++ frontend/src/app/tichu/pages/game-loader/game-loader.module.ts
@@ -1,17 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {GameLoaderPageComponent} from './game-loader-page/game-loader-page.component';
-import {GameLoaderRoutingModule} from "./game-loader-routing.module";
-import {SharedModule} from "../../shared/shared.module";
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { GameLoaderPageComponent } from './game-loader-page/game-loader-page.component'
+import { GameLoaderRoutingModule } from './game-loader-routing.module'
+import { SharedModule } from '../../shared/shared.module'
 
 @NgModule({
   declarations: [
-    GameLoaderPageComponent,
+    GameLoaderPageComponent
   ],
   imports: [
     CommonModule,
     GameLoaderRoutingModule,
-    SharedModule,
+    SharedModule
   ]
 })
 export class GameLoaderModule { }
