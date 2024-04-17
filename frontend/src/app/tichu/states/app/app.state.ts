c888736794405a648759b325856606d27a4418ef
jaunerc <c.jauner@gmail.com>
2024-04-17 07:38:53 +0200

    migrate enums to types

--- frontend/src/app/tichu/states/app/app.state.ts
+++ frontend/src/app/tichu/states/app/app.state.ts
@@ -23,19 +23,9 @@ export interface PlayerState {
 
 export type PlayerSeatId = 'FIRST' | 'SECOND' | 'THIRD' | 'FOURTH'
 
-export enum GamePhaseState {
-  PLAYERS_JOINING = 'PLAYERS_JOINING',
-  DEALING_CARDS = 'DEALING_CARDS',
-  FIRST_EIGHT_CARDS_ARE_DEALT = 'FIRST_EIGHT_CARDS_ARE_DEALT',
-  ALL_CARDS_ARE_DEALT = 'ALL_CARDS_ARE_DEALT',
-  GAME_IS_RUNNING = 'GAME_IS_RUNNING',
-  GAME_ENDED = 'GAME_ENDED'
-}
+export type GamePhaseState = 'PLAYERS_JOINING' | 'DEALING_CARDS' | 'FIRST_EIGHT_CARDS_ARE_DEALT' | 'ALL_CARDS_ARE_DEALT' | 'GAME_IS_RUNNING' | 'GAME_ENDED'
 
-export enum TeamIdentifierState {
-  FIRST_TEAM = 'FIRST_TEAM',
-  SECOND_TEAM = 'SECOND_TEAM'
-}
+export type TeamIdentifierState = 'FIRST_TEAM' | 'SECOND_TEAM'
 
 export interface PlayerPrivateState {
   cards: string[]
--- frontend/src/app/tichu/websocket-api/websocket.api.ts
+++ frontend/src/app/tichu/websocket-api/websocket.api.ts
@@ -23,26 +23,11 @@ export interface PlayerDto {
   teamIdentifierDto: TeamIdentifierDto
 }
 
-export enum TeamIdentifierDto {
-  FIRST_TEAM = 'FIRST_TEAM',
-  SECOND_TEAM = 'SECOND_TEAM'
-}
+export type TeamIdentifierDto = 'FIRST_TEAM' | 'SECOND_TEAM'
 
-export enum PlayerSeatIdDto {
-  FIRST = 'FIRST',
-  SECOND = 'SECOND',
-  THIRD = 'THIRD',
-  FOURTH = 'FOURTH'
-}
+export type PlayerSeatIdDto = 'FIRST' | 'SECOND' | 'THIRD' | 'FOURTH'
 
-export enum GamePhaseDto {
-  PLAYERS_JOINING = 'PLAYERS_JOINING',
-  DEALING_CARDS = 'DEALING_CARDS',
-  FIRST_EIGHT_CARDS_ARE_DEALT = 'FIRST_EIGHT_CARDS_ARE_DEALT',
-  ALL_CARDS_ARE_DEALT = 'ALL_CARDS_ARE_DEALT',
-  GAME_IS_RUNNING = 'GAME_IS_RUNNING',
-  GAME_ENDED = 'GAME_ENDED'
-}
+export type GamePhaseDto = 'PLAYERS_JOINING' | 'DEALING_CARDS' | 'FIRST_EIGHT_CARDS_ARE_DEALT' | 'ALL_CARDS_ARE_DEALT' | 'GAME_IS_RUNNING' | 'GAME_ENDED'
 
 export interface PlayerPrivateMessage {
   playerPrivateState: PlayerPrivateDto
