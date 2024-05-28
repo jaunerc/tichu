1eda66991b09b6762fb519814bebbf4d41a89b5d
jaunerc <c.jauner@gmail.com>
2024-05-28 16:47:16 +0200

    refactor grand tichu websocket adapter

--- backend/domain/src/main/java/ch/jaunerc/tichu/backend/domain/game/GrandTichuUseCase.java
+++ backend/domain/src/main/java/ch/jaunerc/tichu/backend/domain/game/GrandTichuUseCase.java
@@ -2,10 +2,12 @@ package ch.jaunerc.tichu.backend.domain.game;
 
 import ch.jaunerc.tichu.backend.domain.game.model.Game;
 import ch.jaunerc.tichu.backend.domain.game.model.Player;
+import ch.jaunerc.tichu.backend.domain.game.port.input.DealCardsInputPort;
 import ch.jaunerc.tichu.backend.domain.game.port.input.GrandTichuInputPort;
 import ch.jaunerc.tichu.backend.domain.game.port.output.FindGameByIdOutputPort;
 import ch.jaunerc.tichu.backend.domain.game.port.output.FindPlayerByIdOutputPort;
 import ch.jaunerc.tichu.backend.domain.game.port.output.SavePlayerOutputPort;
+import ch.jaunerc.tichu.backend.domain.game.port.output.SendPlayerPrivateStateOutputPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,14 +20,24 @@ public class GrandTichuUseCase implements GrandTichuInputPort {
     private final FindGameByIdOutputPort findGameByIdPort;
     private final FindPlayerByIdOutputPort findPlayerByIdOutputPort;
     private final SavePlayerOutputPort savePlayerPort;
+    private final DealCardsInputPort dealCardsInputPort;
+    private final SendPlayerPrivateStateOutputPort sendPlayerPrivateStateOutputPort;
 
     @Override
     public Game grandTichuByPlayer(UUID gameId, UUID playerId, boolean isGrandTichuCalled) {
         var player = findPlayerByIdOutputPort.findPlayerById(playerId);
 
-        savePlayerPort.savePlayer(Player.Builder.of(player)
+        var updatedPlayer = Player.Builder.of(player)
                 .grandTichuCalled(isGrandTichuCalled)
-                .build());
+                .build();
+
+        sendPlayerPrivateStateOutputPort.sendPlayerPrivateState(
+                gameId,
+                Player.Builder.of(updatedPlayer)
+                        .cards(dealCardsInputPort.dealCards(gameId, playerId))
+                        .build());
+
+        savePlayerPort.savePlayer(updatedPlayer);
 
         return findGameByIdPort.findGameById(gameId);
     }
---
+++ backend/input-websocket/src/main/java/ch/jaunerc/tichu/backend/websocket/send/SendPlayerPrivateStateWebsocketAdapter.java
@@ -0,0 +1,24 @@
+package ch.jaunerc.tichu.backend.websocket.send;
+
+import ch.jaunerc.tichu.backend.domain.game.model.Player;
+import ch.jaunerc.tichu.backend.domain.game.port.output.SendPlayerPrivateStateOutputPort;
+import ch.jaunerc.tichu.backend.websocket.message.game.PlayerPrivateDtoConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class SendPlayerPrivateStateWebsocketAdapter implements SendPlayerPrivateStateOutputPort {
+
+    private final MessageSenderService messageSenderService;
+
+    @Override
+    public void sendPlayerPrivateState(UUID gameId, Player player) {
+        messageSenderService.sendPlayerPrivateStateMessage(
+                gameId.toString(),
+                player.uuid().toString(),
+                PlayerPrivateDtoConverter.convert(player.cards()));
+    }
+}
