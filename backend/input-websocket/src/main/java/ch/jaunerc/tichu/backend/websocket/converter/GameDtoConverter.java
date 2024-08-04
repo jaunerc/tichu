690a67d4f41d62a91f77a243b34047f499bbcd55
jaunerc <c.jauner@gmail.com>
2024-08-04 10:11:26 +0200

    simplify imports

--- backend/input-websocket/src/main/java/ch/jaunerc/tichu/backend/websocket/converter/GameDtoConverter.java
+++ backend/input-websocket/src/main/java/ch/jaunerc/tichu/backend/websocket/converter/GameDtoConverter.java
@@ -12,12 +12,12 @@ import java.util.stream.Stream;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public final class GameDtoConverter {
 
-    public static ch.jaunerc.tichu.backend.websocket.api.model.GameDto convert(Game game) {
+    public static GameDto convert(Game game) {
         var players = Stream.concat(convertFirstTeamPlayers(game), convertSecondTeamPlayers(game)).toList();
         return new GameDto(players, GameDto.GamePhaseEnum.fromValue(game.gamePhase().name()));
     }
 
-    private static Stream<ch.jaunerc.tichu.backend.websocket.api.model.PlayerDto> convertFirstTeamPlayers(Game game) {
+    private static Stream<PlayerDto> convertFirstTeamPlayers(Game game) {
         return Stream.of(
                 PlayerDtoConverter.convert(game.firstTeam().firstPlayer(), TeamIdentifierEnum.FIRST_TEAM),
                 PlayerDtoConverter.convert(game.firstTeam().secondPlayer(), TeamIdentifierEnum.FIRST_TEAM)
